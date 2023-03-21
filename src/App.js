f68a0bc9da587df398264a3e5b11152851a39bab
HizbullahBhatti <hizbullah.kcsf18@iba-suk.edu.pk>
2023-03-21 22:18:35 +0500

    angrytools gradient navrbar color

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
 import React from 'react';
-
-import { Article, Brand, CTA, Navbar, Feature } from './components/index.js';
+import Navbar from './components/navbar/Navbar';
+import Brand from './components/brand/Brand';
+import Possibility from './containers/possibility/Possibility';
+import CTA from './components/cta/CTA';
+import Blog from './containers/blog/Blog';
+import Footer from './containers/footer/Footer';
+import Header from './containers/header/Header';
+import WhatGPT from './containers/whatGPT3/WhatGPT';
+import './App.css';
+// import { Article, Brand, CTA, Navbar, Feature } from './components/index';
+// import { Blog, Footer, Header, Possibility, WhatGPT } from './containers';
 
 function App() {
   return (
-    <></>
+    
+    <div className="App">
+      <div className="gradient__bg">
+        <Navbar />
+        <Header />
+      </div>
+      <Brand/>
+      <WhatGPT/>
+      <Possibility/>
+      <CTA/>
+      <Blog/>
+      <Footer/>
+    </div>
   );
 }
 
